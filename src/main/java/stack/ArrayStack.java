bba6e6b9f9546986d2d0064d0e2eb532c001f6be
xiaohuanwang <wxh_work@163.com>
2019-11-02 18:53:50 +0800

    commit--队列

--- src/main/java/stack/ArrayStack.java
+++ src/main/java/stack/ArrayStack.java
@@ -12,6 +12,7 @@ public class ArrayStack {
     private int n;          //栈的大小
 
 
+    //初始化数组，申请一个大小为n的数组空间
     public ArrayStack(int n){
         this.items=new String[n];
         this.n=n;
@@ -21,9 +22,11 @@ public class ArrayStack {
 
     //入栈操作
     public boolean push(String item){
+        //数组空间满了，入栈失败
         if(count==n){
             return  false;
         }
+        //将item放到下标为count的位置，并且count加一
         items[count]=item;
         count=count+1;
         return true;
@@ -31,9 +34,11 @@ public class ArrayStack {
 
     //出栈操作
     public String pop(){
+        //数组里面无数据，栈为空，则直接返回null
         if(count==0){
             return  null;
         }
+        //返回下标为count-1的数组元素，并且栈中元素个数count减一
         String tem=items[count-1];
         count=count-1;
         return  tem;
